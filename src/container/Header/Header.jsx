ab111ef9d5e3cd6b10c3f5b21bdccb368220898b
Arslan Hasanov <hasanovarslan01.02.08@gmail.com>
2022-06-23 17:18:33 +0800

    Navbar Part

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.css';
+
+import images from '../../constants/images';
+
+const Header = () => (
+  <div>
+    Header
+  </div>
+);
+
+export default Header;
